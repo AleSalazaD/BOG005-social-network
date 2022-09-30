4cc697a5e750846ac026f80f5ade92397c17ba8c
Monik586 <monik.rodriguez586@gmail.com>
2022-09-30 14:25:18 -0500

    cambios

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -4,24 +4,20 @@ export const welcome = () => {
   const sectionWelcome = document.createElement('section');
   sectionWelcome.className = 'sectionWelcome';
 
-  const imgWelcome = document.createElement('img');
-  imgWelcome.className = 'img-welcome';
-  imgWelcome.src = 'img/NomeLogo.png';
-  imgWelcome.alt = 'Name Logo';
-
-  const message = document.createElement('h2');
-  message.className = 'description';
-  message.textContent = 'Disfruta de las diferentes festividades alrededor del mundo.';
+  // const message = document.createElement('h2');
+  // message.className = 'description';
+  // message.textContent = 'Disfruta de las diferentes festividades alrededor del mundo.';
 
   const buttonStart = document.createElement('button');
   buttonStart.className = 'button';
   buttonStart.textContent = 'Inicia tu viaje';
+  buttonStart.id = 'button-start';
 
   buttonStart.addEventListener('click', () => {
     onNavigate('/landing');
   });
 
-  sectionWelcome.append(imgWelcome, message, buttonStart);
+  sectionWelcome.append( buttonStart );
 
   return sectionWelcome;
 };
--- src/firebase/__mocks__/connection.js
+++ src/firebase/__mocks__/connection.js
@@ -1,17 +1,55 @@
-/* eslint-disable arrow-body-style */
-/* eslint-disable no-unused-vars */
-export const createUser = (email, password) => {
-  return Promise.resolve();
+// Import the functions you need from the SDKs you need
+import { initializeApp, getAuth,  createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2v7-YRP-DRZyiKnDmrnWnp9TYTSGoliA",
+  authDomain: "pruebas-f-ec13d.firebaseapp.com",
+  projectId: "pruebas-f-ec13d",
+  storageBucket: "pruebas-f-ec13d.appspot.com",
+  messagingSenderId: "951145660608",
+  appId: "1:951145660608:web:5a09e7f5ece3cffd81ad25"
 };
 
-export const signInUser = (email, password) => {
-  return Promise.resolve();
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const createUser= (email, password) => {
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
 };
 
-export const getAuth = () => {
-  return Promise.resolve();
-};
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
 
-export const register = (email, password) => {
-  return Promise.resolve();
-};
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      const uid = user.uid;
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
+
+  export {createUser}
